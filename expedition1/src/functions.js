115a6f8463a42db60c788e512fd02d17df45924c
Natalia <markina_n@resanta.ru>
2023-03-29 14:19:15 +0300

    update

---
+++ expedition1/src/functions.js
@@ -0,0 +1,41 @@
+
+
+//позволяет выбрать самого опытного капитана
+function getRightCaptain() {
+
+}
+
+//позволяет выбрать самого опытного врача среди женщин
+function getRightDoc() {
+
+}
+
+//позволяет выбрать всех бортмехаников
+function getAllEngineer() {
+
+}
+
+//Позволяет отобрать все марсоходы
+function getAllRover(){
+
+}
+
+//позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
+function getRightRovers(){
+
+}
+
+//позволяет выбрать ракету с максимальной дальностью полёта
+function getRightRocket(){
+
+}
+
+
+module.exports = {
+  getRightCaptain,
+  getAllEngineer,
+  getRightDoc,
+  getAllRover,
+  getRightRovers,
+  getRightRocket
+};
