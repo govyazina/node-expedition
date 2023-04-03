3207f7d096ab46268c469ef5c5670a0c7ac352ea
Svetlana Govyazina <govyazina@gmail.com>
2023-04-03 19:38:50 +0300

    getRightCaptain function added

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,41 +1,43 @@
+const fs = require('fs');
+//получаем массив из файла
+const crew = fs.readFileSync('src/crew.txt', 'utf8').split('\n').map(str => str.split(', '));
 
-
-//позволяет выбрать самого опытного капитана
+// позволяет выбрать самого опытного капитана
 function getRightCaptain() {
-
+    const filtred = crew.filter(person => person[2] === 'Капитан').sort((a, b) => +b[3] - +a[3])
+    return filtred[0].join(', ')
 }
 
-//позволяет выбрать самого опытного врача среди женщин
+// позволяет выбрать самого опытного врача среди женщин
 function getRightDoc() {
 
 }
 
-//позволяет выбрать всех бортмехаников
+// позволяет выбрать всех бортмехаников
 function getAllEngineer() {
 
 }
 
-//Позволяет отобрать все марсоходы
-function getAllRover(){
+// Позволяет отобрать все марсоходы
+function getAllRover() {
 
 }
 
-//позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
-function getRightRovers(){
+// позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
+function getRightRovers() {
 
 }
 
-//позволяет выбрать ракету с максимальной дальностью полёта
-function getRightRocket(){
+// позволяет выбрать ракету с максимальной дальностью полёта
+function getRightRocket() {
 
 }
-
 
 module.exports = {
-  getRightCaptain,
-  getAllEngineer,
-  getRightDoc,
-  getAllRover,
-  getRightRovers,
-  getRightRocket
+    getRightCaptain,
+    getAllEngineer,
+    getRightDoc,
+    getAllRover,
+    getRightRovers,
+    getRightRocket,
 };
