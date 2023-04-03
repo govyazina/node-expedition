85209ef1756c30459a1008738207907ed6867e4d
Svetlana Govyazina <govyazina@gmail.com>
2023-04-03 20:18:08 +0300

    something optimized

--- expedition1/src/functions.js
+++ expedition1/src/functions.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 //получаем массив из файла
-const crew = fs.readFileSync('src/crew.txt', 'utf8').split('\n').map(str => str.split(', '));
-const equipment = fs.readFileSync('src/equipment.txt', 'utf8').split('\n').map(str => str.split(', '));
-const rockets = fs.readFileSync('src/rockets.txt', 'utf8').split('\n').map(str => str.split(', '));
+const getArrFromFile = (url) => fs.readFileSync(url, 'utf8').split('\n').map(str => str.split(',').map(str => str.trim()))
+
+const crew = getArrFromFile('src/crew.txt')
+const equipment = getArrFromFile('src/equipment.txt')
+const rockets = getArrFromFile('src/rockets.txt')
+
 
 // позволяет выбрать самого опытного капитана
 function getRightCaptain() {
@@ -19,20 +22,17 @@ function getRightDoc() {
 
 // позволяет выбрать всех бортмехаников
 function getAllEngineer() {
-    const filtred = crew.filter(person => person[2] === 'Бортмеханик').map(el => el.join(', '))
-    return filtred
+    return crew.filter(person => person[2] === 'Бортмеханик').map(el => el.join(', '))
 }
 
 // Позволяет отобрать все марсоходы
 function getAllRover() {
-    const filtred = equipment.filter(rover => rover[1] === 'марсоход').map(el => el.join(', '))
-    return filtred
+    return equipment.filter(rover => rover[1] === 'марсоход').map(el => el.join(', '))
 }
 
 // позволяет выбрать только те марсоходы, которые смогут прослужить больше 3 лет
 function getRightRovers() {
-    const filtred = equipment.filter(rover => rover[1] === 'марсоход' && +rover[2] > 3).map(el => el.join(', '))
-    return filtred
+    return equipment.filter(rover => rover[1] === 'марсоход' && +rover[2] > 3).map(el => el.join(', '))
 }
 
 // позволяет выбрать ракету с максимальной дальностью полёта
